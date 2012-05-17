941d8bf769b67b6b05f39866b8e4b51325a73b96
Serhiy Oplakanets <serhiy@oplakanets.com>
2012-05-17 15:41:07 +0300

    Patch epel recipe to make damn thing work.

--- recipes/epel.rb
+++ recipes/epel.rb
@@ -20,22 +20,31 @@
 major = node['platform_version'].to_i
 epel  = node['yum']['epel_release']
 
-# If rpm installation from a URL supported 302's, we'd just use that.
-# Instead, we get to remote_file then rpm_package.
-
-remote_file "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm" do
-  source "http://download.fedoraproject.org/pub/epel/#{major}/i386/epel-release-#{epel}.noarch.rpm"
+# TODO: This is an ugly hack to make the damn thing work. ORginal code commented below.
+execute "add_epel_repo" do
+  command "rpm -Uhv http://download.fedoraproject.org/pub/epel/#{major}/i386/epel-release-#{epel}.noarch.rpm"
   not_if "rpm -qa | egrep -qx 'epel-release-#{epel}(|.noarch)'"
-  notifies :install, "rpm_package[epel-release]", :immediately
 end
 
-rpm_package "epel-release" do
-  source "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm"
-  only_if {::File.exists?("#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm")}
-  action :nothing
-end
 
-file "epel-release-cleanup" do
-  path "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm"
-  action :delete
-end
+# ORIGINAL CODE:
+
+## If rpm installation from a URL supported 302's, we'd just use that.
+## Instead, we get to remote_file then rpm_package.
+#
+#remote_file "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm" do
+#  source "http://download.fedoraproject.org/pub/epel/#{major}/i386/epel-release-#{epel}.noarch.rpm"
+#  not_if "rpm -qa | egrep -qx 'epel-release-#{epel}(|.noarch)'"
+#  notifies :install, "rpm_package[epel-release]", :immediately
+#end
+#
+#rpm_package "epel-release" do
+#  source "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm"
+#  only_if {::File.exists?("#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm")}
+#  action :nothing
+#end
+#
+#file "epel-release-cleanup" do
+#  path "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm"
+#  action :delete
+#end
